f2717b6be2c97022d50c12554061aa8fadc25441
xezon <4720891+xezon@users.noreply.github.com>
2023-04-27 18:27:35 +0200

    Add original sample project files

---
+++ Project/Scripts/Python/OnPreBuildItem.py
@@ -0,0 +1,10 @@
+# This python script is called on build via ModBundleItems.json configuration
+
+def OnPreBuild(**kwargs) -> None:
+    print("OnPreBuildItem.py called ...")
+
+    bundleItem = kwargs.get("_bundleItem")
+    info = kwargs.get("info")
+
+    assert bundleItem != None, "_bundleItem kwargs not found"
+    assert info != None, "info kwargs not found"
---
+++ Project/Scripts/Python/OnRunPack.py
@@ -0,0 +1,10 @@
+# This python script is called on build via ModBundlePacks.json configuration
+
+def OnEvent(**kwargs) -> None:
+    print("OnRunPack.py called ...")
+
+    rawBuildThing = kwargs.get("_rawBuildThing")
+    releaseBuildThing = kwargs.get("_releaseBuildThing")
+
+    assert rawBuildThing != None, "_rawBuildThing kwargs not found"
+    assert releaseBuildThing != None, "_releaseBuildThing kwargs not found"
